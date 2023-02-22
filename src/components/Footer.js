19726638aa1205bb55d600f631f3f1a116e85b97
Renzo Marangon <renzomarangon@gmail.com>
2023-02-22 01:23:02 -0300

    v1.3 | responsive view added

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='footer__container'>
+        <a href='#'> 
+          <img src='up.svg' />
+        </a>
+    </div>
+  )
+}
