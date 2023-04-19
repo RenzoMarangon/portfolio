986755a11beb690e3128459433b7d9af47949221
Renzo <81322658+RenzoMarangon@users.noreply.github.com>
2023-04-19 20:02:36 -0300

    v1.3.4 | wowjs + animate.css

--- src/helpers/wowjs.js
+++
@@ -1,9 +0,0 @@
-import WOW from 'wowjs';
-
-export const wowx = new WOW.WOW({
-    boxClass:     'wow',      // default
-    animateClass: 'animated', // default
-    offset:       100,          // default
-    mobile:       true,       // default
-    live:         true        // default
-  })
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore, combineReducers, applyMiddleware} from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
-import navReducer from "./reducers/navReducer";
-
-const reducer = combineReducers({
-    menu: navReducer,
-})
-
-export const store = configureStore({reducer}, applyMiddleware(thunk));
